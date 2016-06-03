b7a6e2ffeb319c8194fcf8abb0181331da112fb2
hugo <hugowangchn@gmail.com>
2016-06-03 16:43:53 +1200

    bug fix

--- resources/views/killprice.blade.php
+++ resources/views/killprice.blade.php
@@ -23,7 +23,8 @@
         {!! Form::close() !!}
 
         @if(!empty($data))
-            <table class="table table-bordered" ng-app="myApp" ng-controller="customersCtrl">
+            {{--<table class="table table-bordered" ng-app="myApp" ng-controller="customersCtrl">--}}
+            <table class="table table-bordered" ng-app="">
                 <tr>
                     {{--<td class="col-md-1">Code</td>--}}
                     <td class="col-md-4">Name</td>
