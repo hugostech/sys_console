40a6cdd5a9774e010b675e54452fe351e92370bd
hugostech <hugowangchn@gmail.com>
2016-06-08 16:14:46 +1200

    add fillable variable in product

--- app/Ex_product.php
+++ app/Ex_product.php
@@ -9,10 +9,15 @@ class Ex_product extends Model
     protected $connection = 'extremepc_mysql';
     protected $table = 'oc_ex_product';
     protected $primaryKey = 'product_id';
+    protected $fillable = array(
+        'model', 'quantity', 'stock_status_id', 'shipping', 'price',
+        'tax_class_id', 'weight', 'weight_class_id', 'subtract', 'sort_order', 'status'
+    );
     public $timestamps = false;
+
 
     public function special()
     {
-        return $this->hasOne('App\Ex_speceal','product_id');
+        return $this->hasOne('App\Ex_speceal', 'product_id');
     }
 }
