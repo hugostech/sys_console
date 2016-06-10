16384b50d824b5be71ade2ece786a87609e3fac8
hugo <hugowangchn@gmail.com>
2016-06-10 16:32:02 +1200

    add preload page

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -8,6 +8,31 @@
     <script src="{{url('',['js','bootstrap.min.js'])}}"></script>
     <script src="{{url('',['js','special.js'])}}"></script>
     <script src="{{url('',['js','angular.min.js'])}}"></script>
+    <style>
+
+        /* Paste this css to your style sheet file or under head tag */
+        /* This only works with JavaScript,
+        if it's not present, don't show loader */
+        .no-js #loader { display: none;  }
+        .js #loader { display: block; position: absolute; left: 100px; top: 0; }
+        .se-pre-con {
+            position: fixed;
+            left: 0px;
+            top: 0px;
+            width: 100%;
+            height: 100%;
+            z-index: 9999;
+            background: url("{{url('/',['iamge','Preloader.gif'])}}") center no-repeat #fff;
+        }
+    </style>
+    <script>
+    //paste this code under the head tag or in a separate js file.
+    // Wait for window load
+    $(window).load(function() {
+    // Animate loader off screen
+    $(".se-pre-con").fadeOut("slow");;
+    });
+    </script>
 
 </head>
 <body>
@@ -81,4 +106,5 @@
     </div>
 
 </body>
+<div class="se-pre-con"></div>
 </html>
