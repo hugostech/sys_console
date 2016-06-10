f308c6f3e4c7af25b93896fff200b3251d39ae81
hugo <hugowangchn@gmail.com>
2016-06-10 16:52:03 +1200

    add sync all products link

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -83,6 +83,7 @@
                                     <li><a href="{{url('/',['killprice'])}}">Kill price</a></li>
                                     <li><a href="{{url('/',['sync'])}}">Sync</a></li>
                                     <li><a href="{{url('/',['syncproall'])}}">SyncAllProduct</a></li>
+                                    <li><a href="{{url('/',['syncpro'])}}">SyncSingleProduct</a></li>
                                     {{--<li><a href="{{url('/',['self_check'])}}">self_check</a></li>--}}
                                     <li><a href="http://italker.info/lunch">Lunch</a></li>
                                     <li><a href="{{url('/',['supplier'])}}">new Supplier</a></li>
