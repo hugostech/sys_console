21b60d80d28f838e2547303011965e27222302f6
hugo <hugowangchn@gmail.com>
2016-06-23 13:04:26 +1200

    bug fix

--- app/Http/Controllers/unilityController.php
+++ app/Http/Controllers/unilityController.php
@@ -391,7 +391,7 @@ class unilityController extends Controller
         );
         foreach($orders as $order){
             $historys = $order->historys->last();
-            dd($historys);
+//            dd($historys);
             $status = $historys->order_status_id;
 
             if(in_array($status,$reminderStatus)){
