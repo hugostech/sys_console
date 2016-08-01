a3488306093d3ab396173965c8fec4beb6eff237
hugo <hugowangchn@gmail.com>
2016-08-01 14:10:31 +1200

    bug fix

--- app/Ex_stock_status.php
+++ app/Ex_stock_status.php
@@ -9,6 +9,7 @@ class Ex_stock_status extends Model
     protected $connection = 'extremepc_mysql';
     protected $table = 'oc_ex_stock_status';
     protected $primaryKey = 'stock_status_id';
+    public $timestamps = false;
 
     public function products(){
         return $this->hasMany('App\Ex_product','stock_status_id');
