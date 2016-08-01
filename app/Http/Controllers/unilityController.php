42c0cffeb1c618d6f08ff9dea0d0ef8e61a0473c
hugo <hugowangchn@gmail.com>
2016-08-01 14:17:32 +1200

    bug fix

--- app/Http/Controllers/unilityController.php
+++ app/Http/Controllers/unilityController.php
@@ -929,10 +929,8 @@ class unilityController extends Controller
         $products = Ex_product::where('model',$request->input('model'))->get();
         if(count($products)>0){
             $name = 'Pre-Order, Releases On '.$request->input('available_time');
-            $stock_status = Ex_stock_status::where('name',$name)->get();
-            if(count($stock_status)>0){
-                $stock_status = $stock_status[0];
-            }else{
+            $stock_status = Ex_stock_status::where('name',$name)->first();
+            if(!isset($stock_status->name)){
                 $stock_status = new Ex_stock_status();
                 $stock_status->language_id=1;
                 $stock_status->name = $name;
