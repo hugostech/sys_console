fb7574951d9e34dece650e83a4e45ee02a3dc631
hugo <hugowangchn@gmail.com>
2016-08-05 15:27:39 +1200

    turn off preload animate

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -43,10 +43,10 @@
     <script>
     //paste this code under the head tag or in a separate js file.
     // Wait for window load
-    $(window).load(function() {
-    // Animate loader off screen
-    $(".se-pre-con").fadeOut("slow");;
-    });
+//    $(window).load(function() {
+//    // Animate loader off screen
+//    $(".se-pre-con").fadeOut("slow");;
+//    });
     </script>
 
 </head>
