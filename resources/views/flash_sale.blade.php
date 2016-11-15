508ec331fa644a14f7de8f44b54366b42002c6c3
WangHugo <Hugo@WangHugodeMacBook-Pro.local>
2016-11-16 01:02:10 +1300

    add publish button

--- resources/views/flash_sale.blade.php
+++ resources/views/flash_sale.blade.php
@@ -6,6 +6,8 @@
         <div class="panel panel-info">
             <div class="panel-heading">
                 <h3>Flash sale product list</h3>
+                <a class="btn btn-primary" href="{{url('publishFlash')}}">Publish</a>
+                <a class="btn btn-danger" href="{{url('offlineFlash')}}">Offline</a>
             </div>
             <div class="panel-body">
                 <table class="table table-bordered ">
