3cc6a6faf3d29f5f99990e80efc7d77eafef22e1
Hugo <hugowangchn@gmail.com>
2017-01-18 14:51:09 +1300

    label size edit

--- resources/views/label/print.blade.php
+++ resources/views/label/print.blade.php
@@ -74,7 +74,7 @@
             .tape-bottom{
                 background: rgb(000,204,000);
                 background-image: url("{{url('image',['logo.png'])}}") ;
-                background-size: 126px 16px;
+                background-size: 126px 17px;
                 background-repeat: no-repeat;
 
                 border-top: solid 1px black;
@@ -145,7 +145,7 @@
         .tape-bottom{
             background: rgb(000,204,000);
             background-image: url("{{url('image',['logo.png'])}}") ;
-            background-size: 126px 16px;
+            background-size: 126px 17px;
             background-repeat: no-repeat;
 
             border-top: solid 1px black;
