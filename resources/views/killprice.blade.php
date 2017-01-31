a576569f4bf3ee7c90df9957ae9898ea1881d3e8
hugostech <hugowangchn@gmail.com>
2017-01-31 15:57:46 +1300

    'removeLabelFromPrintList'

--- resources/views/killprice.blade.php
+++ resources/views/killprice.blade.php
@@ -134,7 +134,7 @@
         myapp.controller('autoComplete',function($scope){
             $scope.categorys = {!! $categorys !!};
         });
-        @if(!is_null($data['bottom_cost']))
+        @if(!is_null($data))
         function remind(price){
             var bottomPrice = {{$data['bottom_cost']*1.15}};
             var oprice = $(price).val();
