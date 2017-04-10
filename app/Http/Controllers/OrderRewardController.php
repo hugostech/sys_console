eb3f4702d433298771bd6a3bad219073c3b3d64b
Hugo <hugowangchn@gmail.com>
2017-04-10 17:00:46 +1200

    add property table

---
+++ app/Http/Controllers/OrderRewardController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class OrderRewardController extends Controller
+{
+    /*
+     * grab all quantified orders
+     * @param int startpoint
+     * @return array All order*/
+    private function listOrder($start){
+
+    }
+    /*
+     * put credit in client account
+     * @return bool */
+    private function putCredit(){
+
+    }
+
+    /*
+     * grab */
+    public function run(){
+
+    }
+}
---
+++ database/migrations/2017_04_10_165321_create_property_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePropertyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('propertys', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('key');
+            $table->string('value')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('propertys');
+    }
+}
