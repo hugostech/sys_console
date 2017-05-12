5c55233064a92b10efb52bc3ad8ea5847966e73e
Hugo <hugowangchn@gmail.com>
2017-05-13 00:13:25 +1200

    bug trace

--- resources/views/killprice/list.blade.php
+++ resources/views/killprice/list.blade.php
@@ -19,23 +19,23 @@
             @foreach($products as $key=>$product)
                 <tr>
                     <td>{{$key + 1}}</td>
-                    {{--<td>{{\App\Ex_product::find($product->product_id)->description->name}}</td>--}}
+                    <td>{{\App\Ex_product::find($product->product_id)->description->name}}</td>
                     <td>{{$product->model}}</td>
                     <td>${{$product->bottomPrice}}</td>
                     <td>
-                        {{!is_null(\App\Ex_product::find($product->product_id))?'t':'f'}}
-                        {{--@if(is_null(\App\Ex_product::find($product->product_id)->special))--}}
-                            {{--${{ round(\App\Ex_product::find($product->product_id)->price * 1.15,2)}}--}}
-                        {{--@else--}}
-                            {{--${{round(\App\Ex_product::find($product->product_id)->special->price * 1.15,2)}}--}}
-                            {{--@if(round(\App\Ex_product::find($product->product_id)->special->price * 1.15,2)<$product->bottomPrice)--}}
-                                {{--<sup class="text-info">Error</sup>--}}
-                            {{--@elseif(round(\App\Ex_product::find($product->product_id)->special->price * 1.15,2)==$product->bottomPrice)--}}
-                                {{--<sup class="text-primary">Touch Bottom</sup>--}}
-                            {{--@else--}}
-                                {{--<sup class="text-danger">On Sale</sup>--}}
-                            {{--@endif--}}
-                        {{--@endif--}}
+                        {{--{{!is_null(\App\Ex_product::find($product->product_id))?'t':'f'}}--}}
+                        @if(is_null(\App\Ex_product::find($product->product_id)->special))
+                            ${{ round(\App\Ex_product::find($product->product_id)->price * 1.15,2)}}
+                        @else
+                            ${{round(\App\Ex_product::find($product->product_id)->special->price * 1.15,2)}}
+                            @if(round(\App\Ex_product::find($product->product_id)->special->price * 1.15,2)<$product->bottomPrice)
+                                <sup class="text-info">Error</sup>
+                            @elseif(round(\App\Ex_product::find($product->product_id)->special->price * 1.15,2)==$product->bottomPrice)
+                                <sup class="text-primary">Touch Bottom</sup>
+                            @else
+                                <sup class="text-danger">On Sale</sup>
+                            @endif
+                        @endif
 
 
 
