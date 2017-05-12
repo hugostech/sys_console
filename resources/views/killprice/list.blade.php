04a6326d32f08dcd56b54a8a782631562cb1365b
Hugo <hugowangchn@gmail.com>
2017-05-12 23:41:37 +1200

    bug trace

--- resources/views/killprice/list.blade.php
+++ resources/views/killprice/list.blade.php
@@ -23,17 +23,17 @@
                     <td>{{$product->model}}</td>
                     <td>${{$product->bottomPrice}}</td>
                     <td>
-                        @if(is_null(\App\Ex_product::find($product->product_id)->special))
-                            {{--${{ round(\App\Ex_product::find($product->product_id)->price * 1.15,2)}}--}}
-                        {{--@else--}}
-                            {{--${{round(\App\Ex_product::find($product->product_id)->special->price * 1.15,2)}}--}}
-                            {{--@if(round(\App\Ex_product::find($product->product_id)->special->price * 1.15,2)<$product->bottomPrice)--}}
-                                {{--<sup class="text-info">Error</sup>--}}
-                            {{--@elseif(round(\App\Ex_product::find($product->product_id)->special->price * 1.15,2)==$product->bottomPrice)--}}
-                                {{--<sup class="text-primary">Touch Bottom</sup>--}}
-                            {{--@else--}}
-                                {{--<sup class="text-danger">On Sale</sup>--}}
-                            {{--@endif--}}
+                        @if(!isset(\App\Ex_product::find($product->product_id)->special))
+                            ${{ round(\App\Ex_product::find($product->product_id)->price * 1.15,2)}}
+                        @else
+                            ${{round(\App\Ex_product::find($product->product_id)->special->price * 1.15,2)}}
+                            @if(round(\App\Ex_product::find($product->product_id)->special->price * 1.15,2)<$product->bottomPrice)
+                                <sup class="text-info">Error</sup>
+                            @elseif(round(\App\Ex_product::find($product->product_id)->special->price * 1.15,2)==$product->bottomPrice)
+                                <sup class="text-primary">Touch Bottom</sup>
+                            @else
+                                <sup class="text-danger">On Sale</sup>
+                            @endif
                         @endif
 
 
