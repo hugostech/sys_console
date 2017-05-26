0b2fa845f9f719f02745cf4961e7c6f6807ba6af
Hugo <hugowangchn@gmail.com>
2017-05-27 03:20:33 +1200

    auto response

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Input;
@@ -29,7 +30,61 @@ class WechatController extends Controller
 //    }
 
     public function entry(Request $request){
-        echo $request->input('ip');
+        $ip = $request->input('ip');
+        if (in_array($ip,$this->getWcIPs())){
+            $content = $request->input('content');
+            return $this->run($content);
+        }
+    }
+
+    private function run($content){
+        $xml = simplexml_load_file($content, 'SimpleXMLElement', LIBXML_NOCDATA);
+        if($xml->MsgType=='event'){
+            return $this->eventHandle($xml->Event,$xml);
+        }else{
+            return $this->msgHandle($xml);
+        }
+    }
+
+    private function msgHandle($xml){
+
+    }
+
+    private function eventHandle($type,$xml){
+        if ($type=='subscribe'){
+            $to = $xml->ToUserName;
+            $from = $xml->FromUserName;
+            $msg = '感谢您关注SFC快羊国际
+            
+            SFC快羊国际是新西兰中国商品购买及转运服务提供商
+
+专门为新西兰用户提供代购中国商品一站式服务
+
+采购、检验、保存、寄送、售后
+
+所有环节我们解决并负责
+
+坚持为用户解决难题、创造价值之理念
+
+以最具美誉之产品与服务“重新定义新西兰购物”！';
+            return $this->msgGenerator($from,$to,$msg);
+
+
+
+        }
+    }
+
+    private function msgGenerator($to,$from,$msg){
+        $msgTemplate = <<<XMLMESSAGE
+                <xml>
+                <ToUserName><![CDATA[%s]]></ToUserName>
+                <FromUserName><![CDATA[%s]]></FromUserName>
+                <CreateTime>%s</CreateTime>
+                <MsgType><![CDATA[text]]></MsgType>
+                <Content><![CDATA[%s]]></Content>
+                </xml>
+XMLMESSAGE;
+        return sprintf($msgTemplate,$to,$from,Carbon::now(),$msg);
     }
 
 //    private function vaild(){
@@ -39,4 +94,39 @@ class WechatController extends Controller
 //
 //
 //    }
+    private function getWcIPs(){
+        $token = $this->getAccessToken();
+        $url = "https://api.weixin.qq.com/cgi-bin/getcallbackip?access_token=$token";
+        $ips = $this->getContent($url);
+        $ips = \GuzzleHttp\json_decode($ips);
+        return $ips['ip_list'];
+    }
+    private function getAccessToken(){
+        $appid = env('WC_APPID');
+        $secret = env('WC_APPSECRET');
+        $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$appid&secret=$secret";
+        $output = $this->getContent($url);
+        return \GuzzleHttp\json_decode($output,true)['access_token'];
+
+    }
+    private function getContent($url)
+    {
+
+        // create curl resource
+        $ch = curl_init();
+
+        // set url
+        curl_setopt($ch, CURLOPT_URL, $url);
+
+        //return the transfer as a string
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        // $output contains the output string
+        $output = curl_exec($ch);
+
+        // close curl resource to free up system resources
+        curl_close($ch);
+
+        return $output;
+    }
 }
