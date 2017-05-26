947a3972c29143748b128a295db539ffc60c6c6f
Hugo <hugowangchn@gmail.com>
2017-05-26 23:54:37 +1200

    bug

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -8,21 +8,22 @@ use Illuminate\Support\Facades\Input;
 
 class WechatController extends Controller
 {
-    private $signature;
-    private $timestamp;
-    private $nonce;
+
     public function index(){
 //        echo Input::get('echostr');
         if(Input::has('echostr')){
-            $this->signature = Input::get('signature');
-            $this->timestamp = Input::get('timestamp');
-            $this->nonce = Input::get('nonce');
-            if ($this->vaild()){
-                return Input::get('echostr');
+            $token = 'sfcwechat';
+            $signature = Input::get('signature');
+            $timestamp = Input::get('timestamp');
+            $nonce = Input::get('nonce');
+            $array = array($token, $timestamp, $nonce);
+            sort($array, SORT_STRING);
+            $str = implode($array);
+            if(sha1($str) == $signature){
+                echo Input::get('echostr');
             }else{
-                return false;
+                echo false;
             }
-
 
         }
     }
@@ -43,16 +44,9 @@ class WechatController extends Controller
     }
 
     private function vaild(){
-        $arr = array(
-            'sfcwechat',
-            $this->timestamp,
-            $this->nonce
-        );
-        sort($arr,SORT_STRING);
-        $str = implode($arr);
-
-
-        return sha1($str)==$this->signature;
+        $array = array(TOKEN, $timestamp, $nonce);
+        sort($array, SORT_STRING);
+        $str = implode($array);
 
 
     }
