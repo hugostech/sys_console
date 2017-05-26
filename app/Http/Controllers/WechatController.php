bb0c751ddeafe5f51586079e3f507bcb57f07c6c
Hugo <hugowangchn@gmail.com>
2017-05-27 00:59:22 +1200

    api

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -9,45 +9,34 @@ use Illuminate\Support\Facades\Input;
 class WechatController extends Controller
 {
 
-    public function index(){
-//        echo Input::get('echostr');
-        if(Input::has('echostr')){
-            $token = 'sfcwechat';
-            $signature = Input::get('signature');
-            $timestamp = Input::get('timestamp');
-            $nonce = Input::get('nonce');
-            $array = array($token, $timestamp, $nonce);
-            sort($array, SORT_STRING);
-            $str = implode($array);
-            if(sha1($str) == $signature){
-                echo Input::get('echostr');
-            }else{
-                echo false;
-            }
-
-        }
-    }
+//    public function index(){
+////        echo Input::get('echostr');
+//        if(Input::has('echostr')){
+//            $token = 'sfcwechat';
+//            $signature = Input::get('signature');
+//            $timestamp = Input::get('timestamp');
+//            $nonce = Input::get('nonce');
+//            $array = array($token, $timestamp, $nonce);
+//            sort($array, SORT_STRING);
+//            $str = implode($array);
+//            if(sha1($str) == $signature){
+//                echo Input::get('echostr');
+//            }else{
+//                echo false;
+//            }
+//
+//        }
+//    }
 
     public function entry(Request $request){
-        if(Input::has('echostr')){
-            $this->signature = Input::get('signature');
-            $this->timestamp = Input::get('timestamp');
-            $this->nonce = Input::get('nonce');
-            if ($this->vaild()){
-                return Input::get('echostr');
-            }else{
-                return false;
-            }
-
-
-        }
+        echo 'tst';
     }
 
-    private function vaild(){
-        $array = array(TOKEN, $timestamp, $nonce);
-        sort($array, SORT_STRING);
-        $str = implode($array);
-
-
-    }
+//    private function vaild(){
+//        $array = array(TOKEN, $timestamp, $nonce);
+//        sort($array, SORT_STRING);
+//        $str = implode($array);
+//
+//
+//    }
 }
