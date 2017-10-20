cdb582bee36657f01b9d1438c716fd9f368ea1cc
Hugo <hugowangchn@gmail.com>
2017-10-21 04:53:44 +1300

    lucydraw pic

--- resources/views/draw/index.blade.php
+++ resources/views/draw/index.blade.php
@@ -9,7 +9,7 @@
 
             <p class="alert alert-success">{{ Session('info') }} <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a></p>
         @endif
-        <h3>Lucky Draw</h3>
+        <h2><a href="http://extremepc.co.nz"><img src="{{url('image',['exlogo.png'])}}"></a> Lucky Draw</h2>
     </div>
     {!! Form::open(['url'=>'/luckyydraw/register','method'=>'post']) !!}
 
@@ -29,6 +29,17 @@
         {!! Form::submit('Sign up',['class'=>'btn btn-primary']) !!}
     </div>
     {!! Form::close() !!}
+    <div class="form-group">
+        <address>
+            <strong>ExtremePC</strong><br>
+            Shop A2, St Lukes Mega Centre<br>
+            1 Wagener Place, St Lukes, Auckland<br>
+            <abbr title="Web">U:</abbr> <a href="http://www.extremepc.co.nz">www.extremepc.co.nz</a><br>
+            <abbr title="Phone">P:</abbr> <a href="tel:+64-9-849-4888">(09) 849-4888</a><br>
+            <abbr title="Email">E:</abbr> <a href="mailto:sales@roctrech.co.nz">sales@roctrech.co.nz</a>
+
+        </address>
+    </div>
     @if(isset($errors))
 
         @if (count($errors) > 0)
