922b047f5581b902b2c8c98a701bf9491b261ed5
hugow-macpro <hugowangchn@gmail.com>
2019-11-28 02:32:58 +1300

    fix

--- app/Http/Controllers/unilityController.php
+++ app/Http/Controllers/unilityController.php
@@ -1228,7 +1228,6 @@ class unilityController extends Controller
         $comment = str_replace("'","^",$order->comment);
         $ship_status = $order->shipping_method == 'Free Shipping' ? 1 : 0;
         $ship_fee = $order->shipfee();
- dd($address2);
         $ship_postcode = $order->shipping_postcode;
         $ship_name = addslashes($order->shipping_firstname.' '.$order->shipping_lastname);
         $data = compact('phone', 'company', 'address1', 'address2', 'city',
