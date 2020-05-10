c7fa8fb3411814a828140b8aeda77d330c271fde
Hugo Wang <hugowangchn@gmail.com>
2020-05-11 02:04:51 +1200

    reminder email command

---
+++ app/Console/Commands/MarketPlaceReminder.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Ex_order;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class MarketPlaceReminder extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'reminder:marketplace 
+                            {order}
+                            {--emailtype= : email type, available: pending-order, review-order}
+                            {--limit=-1 : limit email number }
+                            {--test : output email on screen}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send reminders for marketplace order';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        if ($this->argument('order')){
+            $order = Ex_order::find($this->argument('order'));
+            $this->trigger($order);
+        }else{
+            switch ($this->option('emailtype')){
+                case 'pending-order':
+
+                    break;
+                case 'review-order':
+                    $query = Ex_order::whereHas('status', function ($query){
+                        $query->where('name', 'Complete');
+                    });
+                    break;
+                default:
+                    $query = null;
+                    break;
+            }
+
+
+            $bar = $this->output->createProgressBar($query->count());
+            foreach ($query->get() as $order){
+                $bar->advance();
+            }
+            $bar->finish();
+        }
+    }
+
+    private function trigger(Ex_order $order){
+        switch ($this->option('emailtype')){
+            case 'pending-order':
+                $to = [];
+                break;
+            case 'review-order':
+                break;
+            default:
+                $this->error('Need email type');
+                break;
+        }
+
+
+    }
+
+    /**
+     * @param $to array
+     * format: ['info@email.com', 'user name', 'subject']
+     * @param $template string
+     */
+    private function send($to, $template){
+        if ($this->option('test')){
+            $this->info("send email to $to[1]($to[0]) - $to[0]" );
+            $this->output->block();
+        }else{
+            Mail::send($template, compact('order'), function ($m) use ($to){
+                $m->from('akl.sales@extremepc.co.nz', 'ExtremePC');
+                $m->bcc('tony@extremepc.co.nz', 'Tony Situ');
+                $m->to($to[0],$to[1])->subject($to[2]);
+            });
+        }
+    }
+}
